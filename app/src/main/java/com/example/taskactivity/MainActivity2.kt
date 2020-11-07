4c395314a3535b414dfc38b30cf04d7cc7937814
aaronkyle1115 <aaronkylebutangen@gmail.com>
2020-11-08 00:38:14 +0800

    Final Commit for Task Activity Username: User Password: password

--- app/src/main/java/com/example/taskactivity/MainActivity2.kt
+++ app/src/main/java/com/example/taskactivity/MainActivity2.kt
@@ -1,11 +1,14 @@
 package com.example.taskactivity
 
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import java.util.*
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,32 +17,65 @@ class MainActivity2 : AppCompatActivity() {
 
         var intent: Intent
         var Cam = findViewById<Button>(R.id.btn1)
-        var Gs = findViewById<Button>(R.id.btn2)
-        var Timer = findViewById<Button>(R.id.btn3)
+        var Cal = findViewById<Button>(R.id.btn2)
+        var Web = findViewById<Button>(R.id.btn3)
         var btn4 = findViewById<Button>(R.id.btn4)
         var btn5 = findViewById<Button>(R.id.btn5)
-        val errorDialog = AlertDialog.Builder(this)
-            .setTitle("Error")
-            .setMessage("The intent failed due to not existing")
-            .setNeutralButton("OK") { _, _ ->
-                Toast.makeText(this, "Error", Toast.LENGTH_LONG).show()
-            }
-            .create()
+
 
         Cam.setOnClickListener {
-            intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
+           cam()
+        }
+        Cal.setOnClickListener {
+            Calen()
+        }
+        Web.setOnClickListener{
+            web()
         }
 
         btn4.setOnClickListener {
-            errorDialog.show()
+            errorbox()
         }
 
         btn5.setOnClickListener {
-            errorDialog.show()
+            errorbox()
         }
 
 
     }
+    fun cam(){
+        intent = Intent(this, MainActivity3::class.java)
+        startActivity(intent)
+    }
+
+    fun web() {
+        val net: Intent = Uri.parse("http://wwww.newgrounds.com").let { webpage ->
+            Intent(Intent.ACTION_VIEW, webpage)
+        }
+        startActivity(net)
+    }
+    fun errorbox(){
+        val errorDialog = AlertDialog.Builder(this)
+            .setTitle("Error")
+            .setMessage("The intent failed due to not existing")
+            .setNeutralButton("OK") { _, _ ->
+                Toast.makeText(this@MainActivity2, "Error", Toast.LENGTH_LONG).show()
+            }
+            .create()
+        errorDialog.show()
+    }
+
+    fun Calen(){
+        val year = Calendar.YEAR
+        val month = Calendar.MONTH
+        val day = Calendar.DAY_OF_MONTH
+
+        val datePickerDialog = DatePickerDialog(this@MainActivity2, DatePickerDialog.OnDateSetListener
+        { view, year, monthOfYear, dayOfMonth ->
+
+        }, year, month, day)
+        datePickerDialog.show()
+    }
+
 
 }
